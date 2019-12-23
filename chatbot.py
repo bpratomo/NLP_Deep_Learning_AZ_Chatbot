66f4aa43b3fc4fbd21a6fe39ab09826ffa73bfe2
Budi Pratomo <bpratomo@myrsm.nl>
2019-12-23 16:46:43 +0100

    added chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,38 @@
+### Import Libraries 
+import numpy 
+# import tensorflow as tf 
+import re 
+import time
+
+
+##################################################################
+#### Part 1 - Data Preprocessing 
+#################################################################
+
+
+# Import the dataset
+lines = open('cornell movie-dialogs corpus/movie_lines.txt',encoding='utf-8',errors='ignore',mode='r').read().split('\n')
+print(type(lines))
+conversations = open('cornell movie-dialogs corpus/movie_conversations.txt',encoding='utf-8',errors='ignore',mode='r').read().split('\n')
+print(type(conversations))
+
+
+# Create dictionary of id and line
+id2line = {}
+for line in lines:
+    _line = line.split('+++$+++')
+    if len(_line) ==5:
+        id2line[_line[0].strip()] = _line[4]
+
+print(id2line['L537091'])
+
+# Create list of conversation
+conversation_list = []
+for conversation in conversations:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'","").split(',')
+    conversation_list.append(_conversation)
+
+print(conversation_list[:3])
+
+
+#separate questions and answers
